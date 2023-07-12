ce4b64acaa1e57c2d0eb9d3a776c7775f5ff1f69
Boris IZA <devops@123cloud.cl>
2023-07-12 13:54:17 -0400

    fix caracter additional

--- live/global/sandbox/main.tf
+++ live/global/sandbox/main.tf
@@ -2,7 +2,7 @@
 module "s3_tf_backend" {
   source = "git::https://github.com/BastianCaceres-cloud/CloudInfraKit.git//aws/s3_tf_backend?ref=main"
   bucket_name             = "tf-infraestructure-tfstate-ahpp-sandbox"
-  versioning              = true` #true
+  versioning              = true #true
   force_destroy           = false
   server_side_encryption  = true #true
 }
