8d127a9943e9035578cd4cb89fefb9897ef2504c
BastianCaceres-cloud <76178284+BastianCaceres-cloud@users.noreply.github.com>
2023-07-18 15:36:17 -0400

    Create ec2.tf
    terraform for ec2

---
+++ live/aws/sandbox/sandbox-bastian/main/ec2.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIA6OLYQHP7KNJURDXV"
+  secret_key = "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"
+}
+
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "default" {
+  vpc_id = aws_vpc.default.id
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_instance" "ec2example" {
+  ami = "ami-06ca3ca175f37dd66"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.default.id
+}
