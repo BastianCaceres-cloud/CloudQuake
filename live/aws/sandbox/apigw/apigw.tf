3e4d5cc8daf6eebaa5c75967a20964f225cded27
Boris IZA <devops@123cloud.cl>
2023-07-19 14:40:32 -0400

    add extension to file apigw openapi

--- live/aws/sandbox/apigw/apigw.tf
+++ live/aws/sandbox/apigw/apigw.tf
@@ -1,5 +1,5 @@
 #APIGW - OpenAPI
 module "api_gateway_openapi" {
   source = "git::https://github.com/BastianCaceres-cloud/CloudInfraKit.git//aws/apigw_openapi?ref=main"
-  openapi_definition = file("${path.module}/API-desarrollo-oas30-apigateway")
+  openapi_definition = file("${path.module}/API-desarrollo-oas30-apigateway.yaml")
 }
