49d05f59cfd0792abf5fb50cd1238ac2baec5580
Boris IZA <devops@123cloud.cl>
2023-07-19 14:30:03 -0400

    enable apigw openapi

---
+++ live/aws/sandbox/apigw/apigw.tf
@@ -0,0 +1,5 @@
+#APIGW - OpenAPI
+module "api_gateway_openapi" {
+  source = "git::https://github.com/BastianCaceres-cloud/CloudInfraKit.git//aws/apigw?ref=main"
+  openapi_definition = file("${path.module}/API-desarrollo-oas30-apigateway")
+}
