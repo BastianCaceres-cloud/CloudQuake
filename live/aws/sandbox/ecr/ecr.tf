aadf3cb11c038552f2f3f50ed30102552ca8f67b
Boris IZA <devops@123cloud.cl>
2023-07-24 17:30:36 -0400

    enable ecr

---
+++ live/aws/sandbox/ecr/ecr.tf
@@ -0,0 +1,6 @@
+module "ecr" {
+  source                     = "git::https://github.com/BastianCaceres-cloud/CloudInfraKit.git//aws/ecr?ref=main"
+  ecr_repository_name       = "example-img-lambda-ahpp"
+  ecr_image_tag_mutability  = "MUTABLE"
+  ecr_scan_on_push          = true
+}
