9195533f315e42391717e37d8b66bf0daf053746
Boris IZA <devops@123cloud.cl>
2023-07-25 13:32:49 -0400

    add new lambda for testing

--- live/aws/sandbox/lambdas/lambdas.tf
+++ live/aws/sandbox/lambdas/lambdas.tf
@@ -9,3 +9,16 @@ module "lambda" {
   lambda_memory_size = 128
   # lambda_entry_point = ["/lambda-entrypoint.sh"]
 }
+
+
+
+module "lambda_2" {
+  source = "git::https://github.com/BastianCaceres-cloud/CloudInfraKit.git//aws/lambda?ref=main"
+  
+  lambda_function_name = "my-function-example-fromt-ecr-ahpp2"
+  docker_image_uri = "992927038462.dkr.ecr.us-east-1.amazonaws.com/example-img-lambda-ahpp:latest"
+
+  lambda_timeout    = 3
+  lambda_memory_size = 128
+  # lambda_entry_point = ["/lambda-entrypoint.sh"]
+}
