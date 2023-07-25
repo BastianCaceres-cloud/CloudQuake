ecba06c6a98faceb43fb3225cfebdf546707f0aa
Boris IZA <devops@123cloud.cl>
2023-07-25 13:09:43 -0400

    enable lambdas

---
+++ live/aws/sandbox/lambdas/lambdas.tf
@@ -0,0 +1,11 @@
+
+module "lambda" {
+  source = "git::https://github.com/cloudbuilderspa/CloudInfrakit.git//aws/lambda?ref=feature/aws_lambda"
+  
+  lambda_function_name = "my-function-example-fromt-ecr-ahpp"
+  docker_image_uri = "992927038462.dkr.ecr.us-east-1.amazonaws.com/example-img-lambda-ahpp:latest"
+
+  lambda_timeout    = 3
+  lambda_memory_size = 128
+  # lambda_entry_point = ["/lambda-entrypoint.sh"]
+}
