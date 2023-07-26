d5d250bbb8aeb06c7e20707ff50b139426662e6f
Boris IZA <devops@123cloud.cl>
2023-07-26 14:01:10 -0400

    enable new sg for elasticache

--- live/aws/sandbox/sg/sg.tf
+++ live/aws/sandbox/sg/sg.tf
@@ -1,3 +1,5 @@
+##SECURITY GROUP MSK
+
 module "security_groups" {
   source = "git::https://github.com/BastianCaceres-cloud/CloudInfraKit.git//aws/sg?ref=main"
   vpc_id = "vpc-037adc6ee9cb9654b"
@@ -22,6 +24,47 @@ module "security_groups" {
       egress = [
       {
         description     = "Allow Outbound to MSK Cluster"
+        from_port       = 0
+        to_port         = 0
+        protocol        = "tcp"
+        cidr_blocks     = ["10.0.0.0/16","172.20.0.0/16"]
+        security_groups = [] # Agregar esta propiedad
+      },
+      ]
+    }
+  }
+}
+
+
+
+### SECURITY GROUP ELASTICACHE
+
+
+
+module "security_groups" {
+  source = "git::https://github.com/BastianCaceres-cloud/CloudInfraKit.git//aws/sg?ref=main"
+  vpc_id = "vpc-037adc6ee9cb9654b"
+  security_groups = {
+    "sg-msk-cluster" = {
+      name        = "sg-elasticache-cluster"
+      description = "Security Group for Elasticache Cluster"
+      vpc_id      = "vpc-037adc6ee9cb9654b"
+      tags = {
+        Name = "elasticache-sg"
+      }
+      ingress = [        
+      {
+        description     = "Allow Access to Elasticache Cluster"
+        from_port       = 0
+        to_port         = 6379
+        protocol        = "tcp"
+        cidr_blocks     = ["10.0.0.0/16","172.20.0.0/16"]
+        security_groups = [] # Agregar esta propiedad
+      },
+      ]
+      egress = [
+      {
+        description     = "Allow Outbound to Elasticache Cluster"
         from_port       = 0
         to_port         = 0
         protocol        = "tcp"
