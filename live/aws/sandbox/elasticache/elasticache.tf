d27f88683d01380887a45908744668df7511c5e5
Boris IZA <devops@123cloud.cl>
2023-07-26 14:33:58 -0400

    try other engine redis elasticache

--- live/aws/sandbox/elasticache/elasticache.tf
+++ live/aws/sandbox/elasticache/elasticache.tf
@@ -3,10 +3,10 @@ module "elasticache" {
   cluster_id           = "mi-elasticache-redis-cluster"
   node_type            = "cache.t4g.micro"
   num_cache_nodes      = 1
-  engine_version       = "7.0"
+  engine_version       = "5.0"
   subnet_group_name    = "elasticache-subnetgroup"
   security_group_ids   = ["sg-0f2f2f723b789ee20"]
   subnet_ids           = ["subnet-0a8195b3ebc2d6984", "subnet-094726eea389c7f3a", "subnet-052e519657f5ae177", "subnet-06ad01a8f2a40760a", "subnet-013d49cd18467455d", "subnet-02ab93862b4cedb2c"]
-  parameter_group_family = "redis7.0"
+  parameter_group_family = "redis5.0"
   parameter_group_name = "custom-redis7-group"
 }
