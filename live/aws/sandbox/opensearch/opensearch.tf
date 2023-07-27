b104e057e1fbc48633a7ec48c56853d8c1ba9401
Boris IZA <devops@123cloud.cl>
2023-07-27 13:14:20 -0400

    new name domain  oepnsearch

--- live/aws/sandbox/opensearch/opensearch.tf
+++ live/aws/sandbox/opensearch/opensearch.tf
@@ -2,7 +2,7 @@ module "opensearch_clusteradmin" {
   source = "git::https://github.com/BastianCaceres-cloud/CloudInfraKit.git//aws/opensearch?ref=main"
 
   region        = "us-east-1"
-  domain_name   = "nttdatasandbox-opensearch-domain"
+  domain_name   = "nttdatasandbox-os-domain"
   engine_version = "OpenSearch_1.0"
   instance_type = "m5.large.search"
   instance_count = 1
