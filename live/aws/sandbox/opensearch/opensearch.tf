80079ae85ac89ecc981bc9008ac5b1c2f384df77
Boris IZA <devops@123cloud.cl>
2023-07-27 12:43:52 -0400

    enable opensearch

---
+++ live/aws/sandbox/opensearch/opensearch.tf
@@ -0,0 +1,11 @@
+module "opensearch_clusteradmin" {
+  source = "git::https://github.com/BastianCaceres-cloud/CloudInfraKit.git//aws/opensearch?ref=main"
+
+  region        = "us-east-1"
+  domain_name   = "nttdatasandbox-opensearch-domain"
+  engine_version = "OpenSearch_1.0"
+  instance_type = "m5.large.search"
+  instance_count = 1
+  ebs_enabled   = true
+  volume_size   = 10
+}
