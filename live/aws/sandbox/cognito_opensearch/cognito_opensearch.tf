de202c16b00faed8eef8bfe492310c69361ea17d
Boris IZA <devops@123cloud.cl>
2023-07-27 23:08:05 -0400

    enable new module cognito 1

---
+++ live/aws/sandbox/cognito_opensearch/cognito_opensearch.tf
@@ -0,0 +1,12 @@
+module "cognito_opensearch" {
+  source = "git::https://github.com/BastianCaceres-cloud/CloudInfraKit.git//aws/cognito?ref=main"
+
+  region                = "us-east-1"
+  domain_name           = "nttdatasandbox-os-domain"
+  user_pool_name        = "mi-pool-usuarios-opensearch"
+  user_group_name       = "mi-grupo-usuarios-opensearch"
+  user_group_description = "Grupo de usuarios para OpenSearch"
+  identity_pool_name    = "mi-pool-identidades-opensearch"
+  iam_role_name         = "mi-rol-iam-opensearch"
+  iam_role_policy_name  = "mi-politica-iam-opensearch"
+}
