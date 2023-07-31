9059013327c0f8dde5bfa2a625290dbf016cc322
Boris IZA <devops@123cloud.cl>
2023-07-31 12:13:36 -0400

    user_pool_domain_name

--- live/aws/sandbox/cognito_opensearch/cognito_opensearch.tf
+++ live/aws/sandbox/cognito_opensearch/cognito_opensearch.tf
@@ -9,5 +9,6 @@ module "cognito_opensearch" {
   identity_pool_name    = "mi-pool-identidades-opensearch"
   iam_role_name         = "mi-rol-iam-opensearch"
   iam_role_policy_name  = "mi-politica-iam-opensearch"
-existing_domain_arn     = "arn:aws:es:us-east-1:992927038462:domain/nttdatasandbox-os-domain"
+  existing_domain_arn     = "arn:aws:es:us-east-1:992927038462:domain/nttdatasandbox-os-domain"
+  user_pool_domain_name = "mi-dominio-pool-usuarios-opensearch"
 }
