c276de0e3405d0e8fdfc1296ce0a4b7ca8f356c7
Boris IZA <devops@123cloud.cl>
2023-08-02 12:02:52 -0400

    enable dynamodb + helm bamboo

---
+++ live/aws/sandbox/dynamodb_vault/dynamodb.tf
@@ -0,0 +1,20 @@
+#DYNAMODB FOR HASHICORP VAULT STORAGE BACKEND DYNAMODB WITH EKS FARGATE
+module "vault_dynamodb_backend" {
+  source = "git::https://github.com/BastianCaceres-cloud/CloudInfraKit.git//aws/cognito?ref=main"
+
+  table_name        = "vault-data"
+  
+  read_capacity     = 5
+  write_capacity    = 5
+  
+  partition_key_name = "Path"
+  partition_key_type = "S"
+
+  sort_key_name     = "Key"
+  sort_key_type     = "S"
+  
+  tags              = {
+    Name        = "vault-dynamodb-table"
+    Environment = "prod"
+  }
+}
