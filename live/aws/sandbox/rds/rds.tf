e54c7feaed85d5fbee5319bedd5057574c392a11
Boris IZA <devops@123cloud.cl>
2023-08-10 11:43:11 -0400

    disabled rds bitbucket

--- live/aws/sandbox/rds/rds.tf
+++ live/aws/sandbox/rds/rds.tf
@@ -1,75 +1,75 @@
-module "db" {
-  source = "terraform-aws-modules/rds/aws"
+# module "db" {
+#   source = "terraform-aws-modules/rds/aws"
 
-  identifier = "bitbucketdb"
+#   identifier = "bitbucketdb"
 
-  engine            = "postgres"
-  engine_version    = "14"
-  instance_class    = "db.m5.large"
-  allocated_storage = 30
-  db_name  = "bitbucket"
-  username = "bitbucket"
-  port     = "3306"
+#   engine            = "postgres"
+#   engine_version    = "14"
+#   instance_class    = "db.m5.large"
+#   allocated_storage = 30
+#   db_name  = "bitbucket"
+#   username = "bitbucket"
+#   port     = "3306"
 
-  iam_database_authentication_enabled = true
+#   iam_database_authentication_enabled = true
 
-  vpc_security_group_ids = ["sg-071515f0eb71c8f0d"]
+#   vpc_security_group_ids = ["sg-071515f0eb71c8f0d"]
 
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  backup_window      = "03:00-06:00"
+#   maintenance_window = "Mon:00:00-Mon:03:00"
+#   backup_window      = "03:00-06:00"
 
-  # Enhanced Monitoring - see example for details on how to create the role
-  # by yourself, in case you don't want to create it automatically
-  monitoring_interval    = "30"
-  monitoring_role_name   = "MyRDSMonitoringRole"
-  create_monitoring_role = true
+#   # Enhanced Monitoring - see example for details on how to create the role
+#   # by yourself, in case you don't want to create it automatically
+#   monitoring_interval    = "30"
+#   monitoring_role_name   = "MyRDSMonitoringRole"
+#   create_monitoring_role = true
 
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
+#   tags = {
+#     Owner       = "user"
+#     Environment = "dev"
+#   }
 
-  # DB subnet group
-  create_db_subnet_group = true
-  subnet_ids             = ["subnet-094726eea389c7f3a", "subnet-052e519657f5ae177", "subnet-02ab93862b4cedb2c"]
+#   # DB subnet group
+#   create_db_subnet_group = true
+#   subnet_ids             = ["subnet-094726eea389c7f3a", "subnet-052e519657f5ae177", "subnet-02ab93862b4cedb2c"]
 
-  # DB parameter group
-  family = "postgres14"
+#   # DB parameter group
+#   family = "postgres14"
 
-  # DB option group
-  major_engine_version = "14"
+#   # DB option group
+#   major_engine_version = "14"
 
-  # Database Deletion Protection
-  deletion_protection = false
+#   # Database Deletion Protection
+#   deletion_protection = false
 
-  parameters = [
-    {
-      name  = "autovacuum"
-      value = "1"
-    },
-    {
-      name  = "client_encoding"
-      value = "utf8"
-    }
-  ]
+#   parameters = [
+#     {
+#       name  = "autovacuum"
+#       value = "1"
+#     },
+#     {
+#       name  = "client_encoding"
+#       value = "utf8"
+#     }
+#   ]
 
-  # options = [
-  #   {
-  #     option_name = "MARIADB_AUDIT_PLUGIN"
+#   # options = [
+#   #   {
+#   #     option_name = "MARIADB_AUDIT_PLUGIN"
 
-  #     option_settings = [
-  #       {
-  #         name  = "SERVER_AUDIT_EVENTS"
-  #         value = "CONNECT"
-  #       },
-  #       {
-  #         name  = "SERVER_AUDIT_FILE_ROTATIONS"
-  #         value = "37"
-  #       },
-  #     ]
-  #   },
-  # ]
-}
+#   #     option_settings = [
+#   #       {
+#   #         name  = "SERVER_AUDIT_EVENTS"
+#   #         value = "CONNECT"
+#   #       },
+#   #       {
+#   #         name  = "SERVER_AUDIT_FILE_ROTATIONS"
+#   #         value = "37"
+#   #       },
+#   #     ]
+#   #   },
+#   # ]
+# }
 
 
 
