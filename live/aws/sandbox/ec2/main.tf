3a9b5e4f3e1e974e4ea8c61a77a6501b08768c2b
Boris IZA <devops@123cloud.cl>
2023-08-11 11:15:38 -0400

    enable ec2 for agent

---
+++ live/aws/sandbox/ec2/main.tf
@@ -0,0 +1,16 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "bamboo-agent"
+
+  instance_type          = "m2.2xlarge"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-0142e9d7c1121ebfb"]
+  subnet_id              = "subnet-052e519657f5ae177"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "sandbox"
+  }
+}
