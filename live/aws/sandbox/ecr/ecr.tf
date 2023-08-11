925b08e1e8acc6e8ac27a0777266631652c30302
Boris IZA <devops@123cloud.cl>
2023-08-11 10:37:32 -0400

    enable new ecr arq back

--- live/aws/sandbox/ecr/ecr.tf
+++ live/aws/sandbox/ecr/ecr.tf
@@ -4,3 +4,12 @@ module "ecr" {
   ecr_image_tag_mutability  = "MUTABLE"
   ecr_scan_on_push          = true
 }
+
+
+
+module "ecr_arquetype_back" {
+  source                     = "git::https://github.com/BastianCaceres-cloud/CloudInfraKit.git//aws/ecr?ref=main"
+  ecr_repository_name       = "arquetype-back"
+  ecr_image_tag_mutability  = "MUTABLE"
+  ecr_scan_on_push          = true
+}
