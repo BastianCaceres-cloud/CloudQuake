b4bf63b18bea988b1c1d24886f27c7645bedd394
Boris IZA <devops@123cloud.cl>
2023-08-11 11:18:37 -0400

    add new key pair agent bamboo

--- live/aws/sandbox/key_pair/keypair.tf
+++ live/aws/sandbox/key_pair/keypair.tf
@@ -4,3 +4,11 @@ module "key_pair" {
   key_name           = "key-pair-jfrog"
   create_private_key = true
 }
+
+
+module "key_pair" {
+  source = "terraform-aws-modules/key-pair/aws"
+
+  key_name           = "key-pair-ec2-bamboo-agent"
+  create_private_key = true
+}
