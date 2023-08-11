ff4ef1076b19015a8c3c729731dc5fca3cf68a8a
Boris IZA <devops@123cloud.cl>
2023-08-11 11:18:59 -0400

    add key pair ec2 bamboo agent

--- live/aws/sandbox/ec2/main.tf
+++ live/aws/sandbox/ec2/main.tf
@@ -4,7 +4,7 @@ module "ec2_instance" {
   name = "bamboo-agent"
 
   instance_type          = "m2.2xlarge"
-  key_name               = "user1"
+  key_name               = "key-pair-ec2-bamboo-agent"
   monitoring             = true
   vpc_security_group_ids = ["sg-0142e9d7c1121ebfb"]
   subnet_id              = "subnet-052e519657f5ae177"
