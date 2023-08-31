5b2f0b1ee66029ca715d4dc51cb326411e2f399c
bzamora <boris@cloudbuilder.cl>
2023-08-31 12:56:21 -0400

    add alb controller

--- live/aws/sandbox/alb-controller/alb-controller.tf
+++ live/aws/sandbox/alb-controller/alb-controller.tf
@@ -1,6 +1,6 @@
 module "aws_load_balancer_controller" {
   source = "git::https://github.com/BastianCaceres-cloud/CloudInfraKit.git//aws/alb-controller?ref=main"
-  cluster_name          = "eks-cluster-ahpp-sandbox"
-  cluster_iam_role_name = "eks-cluster-ahpp-sandbox-role-irsa"
+  cluster_name          = "pagopensiones-sandbox"
+  cluster_iam_role_name = "eks-pagopensiones-sandbox-role-irsa"
   iam_policy_file       = "iam-policy.json"
 }
