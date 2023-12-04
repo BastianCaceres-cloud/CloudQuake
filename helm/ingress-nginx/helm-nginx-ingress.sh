6b824ad9811b8f667b07335b39ad93d68e995c4d
asure-sky <sre@123cloud.cl>
2023-12-04 15:15:03 -0300

    add nginx ingress controller
    a

---
+++ helm/ingress-nginx/helm-nginx-ingress.sh
@@ -0,0 +1,3 @@
+helm install ingress-nginx -f values.yaml . -n pagopension
+
+# helm upgrade jenkinsci -f values.yaml .
---
+++ helm/ingress-nginx/helm-u-nginx-ingress.sh
@@ -0,0 +1,3 @@
+helm uninstall ingress-nginx -n pagopension
+
+# helm upgrade jenkinsci -f values.yaml .
